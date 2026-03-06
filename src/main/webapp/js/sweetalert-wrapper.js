d799bd7de397c632076cb717c2f7d19f28d12370
pvthushan <thushanmaduranga71@gmail.com>
2026-03-06 23:25:58 +0530

    Complete the sweetalert-wrapper js file for the frontend page

---
+++ src/main/webapp/js/sweetalert-wrapper.js
@@ -0,0 +1,31 @@
+
+(function() {
+    if (typeof Swal === 'undefined') {
+        console.warn('SweetAlert2 (Swal) is not loaded. Please include the CDN script before this file.');
+        return;
+    }
+
+    window.showAlert = function(message, type) {
+        let icon = 'info';
+        if (!type) type = 'info';
+        if (type === 'success') icon = 'success';
+        else if (type === 'danger' || type === 'error') icon = 'error';
+        else if (type === 'warning') icon = 'warning';
+        else if (type === 'info') icon = 'info';
+
+        Swal.fire({
+            toast: true,
+            position: 'top-end',
+            icon: icon,
+            title: message,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            customClass: {
+                popup: 'swal2-toast-custom'
+            }
+        });
+    };
+
+})();
+
